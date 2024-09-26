2e20b1697f037c1ece195342b8fd23dca3a9806f
Saurabh Patel <sp993672@gmail.com>
2024-09-27 00:11:00 +0530

    Add files via upload

---
+++ print add mul sub divisoin in screen.cpp
@@ -0,0 +1,18 @@
+//Saurabh Patel
+#include<iostream>
+using namespace std;
+int main(){
+	float a,b,c,d,e,f;
+	cout<<"Enter the value 1  =";
+	cin>>a;
+	cout<<"Enter the value 2  =";
+	cin>>b;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	cout<<"Additoin ="<<c<<endl;
+	cout<<"Subtraction ="<<d<<endl;
+	cout<<"Multiplication ="<<e<<endl;
+	cout<<"Divosion ="<<f<<endl;
+}
