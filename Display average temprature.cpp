69829422a98f53c53f3b7e5f79b2b80a19416aff
Saurabh Patel <sp993672@gmail.com>
2024-09-27 00:08:54 +0530

    Add files via upload

---
+++ Display average temprature.cpp
@@ -0,0 +1,21 @@
+//Saurabh Patel 
+#include<iostream>
+using namespace std;
+int main(){
+	float a,b,c,d,e,f,g;
+	cout<<"Day Temerature of Surat  =";
+	cin>>a;
+	cout<<"Day Tumprature of Rajkot =";
+	cin>>b;
+	cout<<"Day Temerature of Gadhinagar =";
+	cin>>c;
+	cout<<"Day Temerature of ahemdabad =";
+	cin>>d;
+	cout<<"Day Temerature of Vadodra =";
+	cin>>e;
+	f=a+b+c+d+e;
+	g=f/5;
+	cout<<"sum is ="<<f<<endl;
+    cout<<"Avrage is ="<<g<<endl;
+	
+}
