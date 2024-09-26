4193316b21224d156fdbb12637f47d7011d0919e
Saurabh Patel <sp993672@gmail.com>
2024-09-27 00:07:51 +0530

    Add files via upload

---
+++ Total and average of a number.cpp
@@ -0,0 +1,13 @@
+//Saurabh Patel
+#include<iostream>
+using namespace std;
+int main(){
+int a,b,c,d,e;
+cin>>a;
+cin>>b;
+cin>>c;
+d=a+b+c;
+e=d/3;
+cout<<"sum is ="<<d<<endl;
+cout<<"Avrage is ="<<e<<endl;
+}
