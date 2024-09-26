4e3aa51a9d577f6d42b7a2270fbbbc52409bd03c
Saurabh Patel <sp993672@gmail.com>
2024-09-27 00:13:23 +0530

    Add files via upload

---
+++ Change tamparature.cpp
@@ -0,0 +1,13 @@
+//Saurabh Patel
+#include<iostream>
+using namespace std;
+int main(){
+	float a,b,c,d;
+	cout<<"Enter the temprature in fahrenheit =";
+	cin>>a;
+	b=a-32;
+	c=b*5;
+	d=c/9;
+	cout<<"The temprature in centigrade ="<<d;
+	
+}
