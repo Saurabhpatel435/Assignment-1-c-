c0082a449cd74e8eeed4fb6cfa30d957e2083d24
Saurabh Patel <sp993672@gmail.com>
2024-09-27 00:11:56 +0530

    Add files via upload

---
+++ Percentage of 5 subject.cpp
@@ -0,0 +1,20 @@
+//Saurabh Patel
+#include<iostream>
+using namespace std;
+int main(){
+	float a,b,c,d,e,f,g;
+	cout<<"Mark in math  =";
+	cin>>a;
+	cout<<"Mark in English  =";
+	cin>>b;
+	cout<<"Mark in Science  =";
+	cin>>c;
+	cout<<"Mark in Hindi  =";
+	cin>>d;
+	cout<<"Mark in Computer  =";
+	cin>>e;
+	f=a+b+c+d+e;
+	cout<<"Total marks got ="<<f<<endl;
+	g=f/5;
+	cout<<"Average marks got ="<<g<<endl;
+}
