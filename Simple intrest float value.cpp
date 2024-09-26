c8c94665a967d4fe3152d4faf704b3deede77c81
Saurabh Patel <sp993672@gmail.com>
2024-09-27 00:09:40 +0530

    Add files via upload

---
+++ Simple intrest float value.cpp
@@ -0,0 +1,16 @@
+//Saurabh Patel
+#include<iostream>
+using namespace std;
+int main(){
+	float p,r,n,a,si;
+	cout<<"Principal =";
+	cin>>p;
+	cout<<"Rate of intrest =";
+	cin>>r; 
+	cout<<"Time =";
+	cin>>n;
+	a=p+r+n;
+	si=a/100;
+	cout<<"Simple intreset is ="<<si<<endl;
+	
+}
